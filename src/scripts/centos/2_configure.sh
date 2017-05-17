2fb47aa90d18db24c64685c0cfcf7b6c94cb03f3
Nenad Radojevic <nradojevic@evoila.de>
2017-05-17 20:29:34 +0200

    Feature: CentOS Support
    Added support to install ntpd on CentOS
    
    Change run-test.sh script to allow running tests against different
    distributions

---
+++ src/scripts/centos/2_configure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SERVERS=${SERVERS}
+
+cat <<EOF > /etc/ntp.conf
+driftfile /var/lib/ntp/ntp.drift
+statistics loopstats peerstats clockstats
+filegen loopstats file loopstats type day enable
+filegen peerstats file peerstats type day enable
+filegen clockstats file clockstats type day enable
+
+EOF
+
+LENGTH=$(echo ${SERVERS} | /usr/bin/jq 'length')
+LAST_INDEX=$((LENGTH-1))
+
+for I in `seq 0 $LAST_INDEX`; do
+  ADDR=$(echo $SERVERS | /usr/bin/jq -r ".[$I]" )
+
+  cat <<EOF >> /etc/ntp.conf
+pool $ADDR iburst
+EOF
+done
+
+cat <<EOF >> /etc/ntp.conf
+
+restrict -4 default kod notrap nomodify nopeer noquery limited
+restrict -6 default kod notrap nomodify nopeer noquery limited
+
+restrict 127.0.0.1
+restrict ::1
+
+restrict source notrap nomodify noquery
+EOF
+
+service ntpd restart
